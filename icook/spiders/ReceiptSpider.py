dc23bbf90bc30c5d99f2fa68663e42918cff559b
ting <ting@linkwish.com>
2016-05-31 13:00:40 +0800

    only search unfinished category

--- icook/spiders/ReceiptSpider.py
+++ icook/spiders/ReceiptSpider.py
@@ -11,17 +11,45 @@ from mysql.connector import Error
 
 DELAY_PAGE_TIME = 4
 PAT = re.compile('https://icook.tw/categories/[0-9]+')
+
+# TABLE_CATEGORY = 'test_categories'
+TABLE_CATEGORY = 'categories'
+TABLE_RECEIPT = 'receipts'
+
 #source ~/Desktop/scarpy/bin/activate
 
 
 class ReceiptSpider(scrapy.Spider):
     name = "receipt"
 
+    def clear_unfinish_category_receipt():
+        try:
+            conn = mysql.connector.connect(host='localhost',database='icook',user='root',password='root', port='8889')
+            cursor = conn.cursor()
+            query = "SELECT id FROM " + TABLE_CATEGORY + " WHERE finish = %s"
+            cursor.execute(query, (0,))
+
+            rows = cursor.fetchall()
+            ids = [x[0] for x in rows]
+
+            for category_id in ids:
+                query = "DELETE FROM " + TABLE_RECEIPT + " WHERE category_id = %s"
+                cursor.execute(query, (category_id,))
+                conn.commit()
+
+        except Error as e:
+            print(e)
+
+        finally:
+            cursor.close()
+            conn.close()
+
     def query_category_links():
         try:
             conn = mysql.connector.connect(host='localhost',database='icook',user='root',password='root', port='8889')
             cursor = conn.cursor()
-            cursor.execute("SELECT category_link FROM categories ORDER BY id")
+            query = "SELECT category_link FROM " + TABLE_CATEGORY + " WHERE finish = %s"
+            cursor.execute(query, (0,))
 
             rows = cursor.fetchall()
             links = [x[0] for x in rows]
@@ -37,12 +65,12 @@ class ReceiptSpider(scrapy.Spider):
 
     def query_category_id(self, url):
         url = re.findall(PAT, url)[0]
-        print url
+        # print url
 
         try:
             conn = mysql.connector.connect(host='localhost',database='icook',user='root',password='root', port='8889')
             cursor = conn.cursor()
-            query = "SELECT id FROM categories WHERE category_link =  %s"
+            query = "SELECT id FROM " + TABLE_CATEGORY + " WHERE category_link =  %s"
             cursor.execute(query, (url,))
 
             row = cursor.fetchone()
@@ -59,11 +87,24 @@ class ReceiptSpider(scrapy.Spider):
             cursor.close()
             conn.close()
 
+    def set_category_finish(self, category_id):
+        # print category_id
+        try:
+            conn = mysql.connector.connect(host='localhost',database='icook',user='root',password='root', port='8889')
+            cursor = conn.cursor()
+            query = """UPDATE """ + TABLE_CATEGORY + """ SET finish = %s WHERE id = %s """
+            cursor.execute(query, (1,category_id))
+            conn.commit()
 
-    # start_urls = ['https://icook.tw/categories/198' ,'https://icook.tw/categories/457']
+        except Error as e:
+            print(e)
+
+        finally:
+            cursor.close()
+            conn.close()
+
+    clear_unfinish_category_receipt()
     start_urls = query_category_links()
-
-    # query_category_id('https://icook.tw/categories/198')
 
     def parse(self, response):
         res = BeautifulSoup(response.body, "lxml")
@@ -71,7 +112,8 @@ class ReceiptSpider(scrapy.Spider):
 
         next_page = res.select('.next_page > a')
         if not next_page:
-            print 'last page.'
+            self.set_category_finish(self.query_category_id(response.url))
+            print '=====last page.====='
         else:
             next_page_url = 'https://icook.tw' + res.select('.next_page > a')[0]['href']
             time.sleep(DELAY_PAGE_TIME)
